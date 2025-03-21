2cb9e9f76b414943798392d5e6e3dca815fdb8cf
Techiegeek1 <michaelade@mypayledger.io>
2025-03-22 00:27:40 +0100

    ..

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -36,7 +36,7 @@ const Homepage = () => {
             </div>
           </div>
           <div
-            className="flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide snap-x pb-10 max-w-[100%]"
+            className="flex gap-4 overflow-x-auto scroll-smooth scrollbar-hidden scrollbar-hide snap-x pb-10 max-w-[100%]"
             style={{ scrollSnapType: "x mandatory" }}
           >
             {houses.map((house) => (
