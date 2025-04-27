42876a62845fa06a13398645ae952f8d567679a7
Techiegeek1 <michaelade@mypayledger.io>
2025-04-27 15:33:33 +0100

    logo changed

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -20,7 +20,12 @@ const PageNav = () => {
       <ul className="shadow flex items-center min-h-12 py-2 p-3 w-2/3 m-auto bg-white text-black rounded-3xl border border-gray-500 border-1 relative">
         {/* Logo Section (Fixed in Place) */}
         <Link to={"/"} className="flex flex-shrink-0">
-          <img src="/full-logo.png" alt="logo" width={170} height={100} />
+          <img
+            src="/Realestway horizontal.svg"
+            alt="logo"
+            width={170}
+            height={100}
+          />
         </Link>
 
         {/* Mobile Menu Toggle Button */}
@@ -33,7 +38,7 @@ const PageNav = () => {
 
         {/* Navigation Links (Responsive) */}
         <div
-          className={`text-lg font-bold md:font-normal absolute top-full left-0 w-full text-[#100073] bg-white rounded-xl md:static md:w-4/5 md:flex md:ml-10 md:items-center md:justify-between transition-all duration-300 ${
+          className={`text-lg md:font-normal absolute top-full left-0 w-full text-[#100073] bg-white rounded-xl md:static md:w-4/5 md:flex md:ml-10 md:items-center md:justify-between transition-all duration-300 ${
             isOpen ? "block" : "hidden"
           }`}
         >
